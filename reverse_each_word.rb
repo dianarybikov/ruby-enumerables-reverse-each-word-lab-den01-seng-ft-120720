a85102a4eb0eb89ae72f0dbb3ea204b9b5fb2d35
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-24 20:12:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word
-  reverse_each_word = ("Hello there, and how are you?").split
-  reverse_each_word
+  .split
+  reverse_each_wordreverse_each_word = ("Hello there, and how are you?")
   reverse_each_word.each.reverse
